217403269637d1cf6f089b5b7404df814c27d931
Dilraj Sandhu <49254698+DilrajS@users.noreply.github.com>
2024-02-02 12:31:32 -0500

    Create App.py

---
+++ App.py
@@ -0,0 +1,7 @@
+import streamlit as st
+print("Hello World")
+# This is the title of your application, it will be displayed at the top of the app.
+st.title('My Streamlit Application')
+
+# This writes text to your app.
+st.write('Hello World! Welcome to my Streamlit application.')
